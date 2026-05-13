008ad3f138cfcc1b222c8810131d935fbe3afdcc
Landon <landon.wiant09@gmail.com>
2026-05-13 12:04:39 -0400

    Simplified Question class, added NumericQuesion class, added LikertQuestion class.

--- questions.py
+++ questions.py
@@ -1,14 +1,31 @@
 class Question:
-    def __init__(self, promt, qtype="float", min_val=None, max_val=None):
+    def __init__(self, prompt):
         self.prompt = prompt
-        self.qtype = qtype
+    def validate(self, answer):
+        raise NotImplementedError
+class NumericQuestion(Question):
+    def __init__(self, prompt, min_val=None, max_val=None):
+        super().__init__(prompt)
         self.min_val = min_val
         self.max_val = max_val
-
+class LikertQuestion(Question):
+    def __init__(self, prompt, scale=5):
+        super().__init__(prompt)
+        self.scale = {
+            1: "Strongly Disagree",
+            2: "Disagree",
+            3: "Neutral",
+            4: "Agree",
+            5: "Strongly Agree"
+        }
+    
 questions = {
-    "sleep hours": Question(
+    "sleep hours": NumericQuestion(
         prompt = "How many hours of sleep do you get per night?",
         min_val = 0,
         max_val = 24
-    )
+    ),
+    "school satisfaction": LikertQuestion(
+        prompt="I am satisfied with my school experience."
+    ),
 }
