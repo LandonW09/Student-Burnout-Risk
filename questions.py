d9a9941bc33980d7dc0c5cfb510031b9f45262c4
Landon <landon.wiant09@gmail.com>
2026-05-16 12:59:21 -0400

    Finished Likert and Reverse Likert question dictionaries.

--- questions.py
+++ questions.py
@@ -48,4 +48,63 @@ reverse_likert_questions = { #These questions are reversed to align higher score
     "perceived_respect": ReverseLikertQuestion(
         prompt = "I feel respected by my teachers and peers.",
     ),
+    "connection_and_support": ReverseLikertQuestion(
+        prompt = "I feel a sense of connection and support from my friends and family.",
+    ),
+    "help_seeking": ReverseLikertQuestion(
+        prompt = "I feel comfortable seeking help from people I trust.",
+    ),
+    "difficult_times": ReverseLikertQuestion(
+        prompt = "When things get difficult, I have people I can turn to for support.",
+    ),
+    "positive_community": ReverseLikertQuestion(
+        prompt = "I feel like I am part of a positive community.",
+    ),
+    "stress_management": ReverseLikertQuestion(
+        prompt = "I manage my stress in a healthy way.",
+    ),
+    "average_mood": ReverseLikertQuestion(
+        prompt = "I feel like I spend more time in a good or neutral mood than a bad mood.",
+    ),
+    "overcoming_adversity": ReverseLikertQuestion(
+        prompt = "When I face adversity, I am confident in my ability to overcome it.",
+    ),
+    "work_pride": ReverseLikertQuestion(
+        prompt = "I take pride in my work and my ability to succeed.",
+    ),
+    "challenge_seeking": ReverseLikertQuestion(
+        prompt = "I seek challenges so that I can push myself to grow and succeed.",
+    ),
+}
+likert_questions = {
+    "problem_avoidance": LikertQuestion(
+        prompt = "I avoid problems and challenges whenever possible.",
+    ),
+    "critical_mindset": LikertQuestion(
+        prompt = "I am often very critical of myself and others.",
+    ),
+    "social_withdrawal": LikertQuestion(
+        prompt = "When things get difficult, I tend to withdraw from social interactions and activities.",
+    ),
+    "thought_suppression": LikertQuestion(
+        prompt = "I don't usually feel comfortable expressing my feelings and thoughts to others.",
+    ),
+    "passive_coping": LikertQuestion(
+        prompt = "When I am stressed, I cope by distracting myself with passive activities (e.g., scrolling through social media, binge watching TV, etc.)"
+    ),
+    "workload_overwhelm": LikertQuestion(
+        prompt = "I often feel overwhelmed by my workload and responsibilities.",
+    ),
+    "efficacy_dissatisfaction": LikertQuestion(
+        prompt = "I am often dissastisfied with my grades, even when I do well."
+    ),
+    "perfectionism": LikertQuestion(
+        prompt = "I often feel like I need to be perfect in order to succeed, and I am disappointed when I don't meet that standard."
+    ),
+    "pessimistic_outlook": LikertQuestion(
+        prompt = "I often have difficulties maintaining a positive outlook when I have a lot on my plate."
+    ),
+    "self_soubt": LikertQuestion(
+        prompt = "It's hard to believe that I can succeed when I have a lot of work to do."
+    )
 }
